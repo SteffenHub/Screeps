1ac2e1bf928936b621f3d230fb70a6b298fd7c36
Steffen Marbach <steffen.marbach@student.uni-luebeck.de>
2021-12-25 21:46:44 +0100

    Bauerbeiter reparieren ein Ding, wenn es weniger als 50% Leben hat nun vollstaendig und suchen dann erst nach der naechsten arbeit

--- role.Bauerbeiter.js
+++ role.Bauerbeiter.js
@@ -8,26 +8,39 @@ module.exports = {
         Creep.arbeitEinteilen(creep);
 
         //Wenn arbeitet -> arbeit machen
-        if (creep.memory.arbeitet){
+        if (creep.memory.arbeitet){           
             //Extensions bevorzugen
             var extension = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES, { filter: (s) => {return s.structureType == STRUCTURE_EXTENSION}});
-            if(extension != undefined){
+            if(extension != undefined){              
                 this.bauen(creep,extension);
             //Sonst alle anderen Gebaeude    
             }else{
-                var kaputtesGebauede = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (s) => s.hits < (s.hitsMax/2) && s.structureType != STRUCTURE_WALL});
-                //Wenn ein Gebauede mit 50% Leben existiert
-                if(kaputtesGebauede != undefined){
-                    this.reparieren(creep,kaputtesGebauede);
-                //Sonst baue neue Sachen
-                }else{
-                    var gebaeude = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-                    //Wenn es arbeit gibt -> arbeite
-                    if (gebaeude != undefined){
-                        this.bauen(creep,gebaeude);
-                    //Wenn es keine arbeit gibt -> werde upgrader    
+                //Wenn er eine Reparier Auftrag hat
+                if (creep.memory.reparierZiel != undefined){
+                    //Wenn das zu reparierende Gebauede fertig repariert ist
+                    if (Game.getObjectById(creep.memory.reparierZiel.id).hits == Game.getObjectById(creep.memory.reparierZiel.id).hitsMax){     
+                        creep.memory.reparierZiel = undefined;
                     }else{
-                        roleUpgrader.run(creep);
+                        this.reparieren(creep,Game.getObjectById(creep.memory.reparierZiel.id));
+                    }
+                //Wenn es keinen reparier Auftrag gibt -> suche nach einen    
+                }else{
+                    var kaputtesGebauede = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (s) => s.hits < (s.hitsMax/2) && s.structureType != STRUCTURE_WALL});
+
+                    //Wenn ein Gebauede mit 50% Leben existiert
+                    if(kaputtesGebauede != undefined){
+                        creep.memory.reparierZiel = kaputtesGebauede;
+                        this.reparieren(creep,kaputtesGebauede);
+                    //Sonst baue neue Sachen
+                    }else{
+                        var gebaeude = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+                        //Wenn es arbeit gibt -> arbeite
+                        if (gebaeude != undefined){
+                            this.bauen(creep,gebaeude);
+                        //Wenn es keine arbeit gibt -> werde upgrader
+                        }else{
+                            roleUpgrader.run(creep);
+                        }
                     }
                 }
             }
