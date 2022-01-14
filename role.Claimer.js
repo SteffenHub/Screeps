805cda1b11425319254df48c8b18fdf07f67a28a
SteffenHub <myEmail@my.com>
2022-01-14 16:20:05 +0100

    Claimer greifen nun auch Controller an, die reserviert sind

--- role.Claimer.js
+++ role.Claimer.js
@@ -5,7 +5,7 @@ module.exports = {
     run: function(creep){
         //Gehe erstmal in den Ziel Raum
         if (!creep.geheInRaum(creep.memory.zielRaum)){
-            if (creep.room.controller.owner != undefined){
+            if (creep.room.controller.owner != undefined || creep.room.controller.reservation != null){
                 if (creep.room.controller.username != "OR1EQUALS1"){
                     if (creep.attackController(creep.room.controller) == ERR_NOT_IN_RANGE){
                         creep.moveTo(creep.room.controller);
