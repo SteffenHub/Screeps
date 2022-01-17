911f7cf584bb5f10f564bab981f6460262705764
SteffenHub <steffen.marbach@student.uni-luebeck.de>
2022-01-17 11:56:52 +0100

    Angreifer Bewegung in anderen wird ueber die Function in prototype.creep.bewegung realisiert

--- role.Angreifer.js
+++ role.Angreifer.js
@@ -1,28 +1,28 @@
 require('prototype.creep.bewegung')();
 module.exports = {
 
-    run: function(creep){
+    run: function (creep) {
 
         //Erstmal in den Raum gehen, der angeriffen werden soll
-        if (!creep.geheInRaum(creep.memory.zielRaum)){
+        if (!creep.geheZumZielRaum()) {
             //Feindliche creeps angreifen
             var creeps = creep.sucheNaechstenGegner();
             if (creeps != undefined) {
-                if (creep.attack(creeps) == ERR_NOT_IN_RANGE){
+                if (creep.attack(creeps) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(creeps);
                 }
             } else {
                 //Spawn angreifen
                 var spawn = creep.sucheNaechstenSpawn();
                 if (spawn != undefined) {
-                    if (creep.attack(spawn) == ERR_NOT_IN_RANGE){
+                    if (creep.attack(spawn) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(spawn);
                     }
                 } else {
                     //alles andere ausser controller angreifen
                     var gebauede = creep.sucheNaechsteStrukturOhneMauernUndController()
                     if (gebauede != undefined) {
-                        if (creep.attack(gebauede) == ERR_NOT_IN_RANGE){
+                        if (creep.attack(gebauede) == ERR_NOT_IN_RANGE) {
                             creep.moveTo(gebauede);
                         }
                     }
