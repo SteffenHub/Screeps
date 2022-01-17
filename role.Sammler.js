d3a8697116bc742c2d81ced536d23d958a3d27bd
SteffenHub <steffen.marbach@student.uni-luebeck.de>
2022-01-17 11:54:29 +0100

    Wenn ein Sammler nichts mehr zu tun hat, also Spawn und Extensions voll sind, dann wird dieser zum Bauarbeiter

--- role.Sammler.js
+++ role.Sammler.js
@@ -1,28 +1,30 @@
 require('prototype.creep.suchen')();
 
 module.exports = {
-    run: function(creep){
+    run: function (creep) {
 
         //Wenn der Creep Energie dabei hat
-        if (creep.arbeitet()){
+        if (creep.arbeitet()) {
             //Erstmal tower voll machen
             var tower = creep.sucheNaechstenTowerNichtVoll();
-            if (tower != undefined){
+            if (tower != undefined) {
                 creep.energieAbgeben(tower);
-            //Dann Spawn voll machen    
-            }else if (creep.room.find(FIND_MY_SPAWNS, {filter: (s) => s.energy < s.energyCapacity}).length != 0) {
-                    creep.energieAbgeben(creep.room.find(FIND_MY_SPAWNS)[0]);
-            }else{
+                //Dann Spawn voll machen
+            } else if (creep.room.find(FIND_MY_SPAWNS, {filter: (s) => s.energy < s.energyCapacity}).length != 0) {
+                creep.energieAbgeben(creep.room.find(FIND_MY_SPAWNS)[0]);
+            } else {
                 var extension = creep.sucheNaechsteExtensionNichtVoll();
                 //Dann Extensions voll machen
-                if (extension != undefined){
+                if (extension != undefined) {
                     creep.energieAbgeben(extension);
-                }else{
-                    //Container voll machen oder so
+                } else {
+                    //Wenn genug Sammler und Bauerbeiter -> Sammler werden zu Bauerbeitern
+                    const roleBauerbeiter = require("./role.Bauerbeiter");
+                    roleBauerbeiter.run(creep);
                 }
             }
-        //Der Creep hat keine Energie mehr    
-        }else{
+        //Der Creep hat keine Energie mehr
+        } else {
             creep.holeEnergie();
         }
     }
